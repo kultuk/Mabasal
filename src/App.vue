24f38500cc970037af5029e83c9a5000cfb36a4a
kultuk <kultukdude@gmail.com>
2020-01-12 23:38:02 +0200

    added dist folder

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <violationList></violationList> -->
+    <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Nuri's Mabasal
+        </h1>
+      </div>
+    </div>
+  </section>
+    <b-input type="text" v-model="filterText"></b-input>
+    <table>
+      <tr v-for="violation in filterdViolations" :key="violation.ID" >
+        <td><span>{{violation.Name}}</span></td>
+
+        <!-- <b-taglist attached style="display:inline-block;"> -->
+        <td>
+
+          <b-tag rounded>{{violation.CatName}}</b-tag>
+        </td>
+        <td>
+          <b-tag rounded type="is-primary" :class="[violation.IsIncluded ? 'is-primary' : 'is-danger']">{{violation.IsIncluded ? 'כלול' : 'לא כלול'}}</b-tag>
+        </td>
+        
+      </tr>
+    </table>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import violationList from './components/violationList.vue'
+
+// var violations = [];
+// Vue.component();
 
 export default {
   name: 'app',
+  created:function(){
+      fetch("http://nuri.dooble.ws/nuriviolations").then(res=>{
+          res.json().then(data=>{
+              // console.log(data)
+              this.violations = data;
+              this.filterdViolations = this.violations.map(x=>x)
+          })
+      })
+  },
+  data: function(){
+    return {violations:[], filterText:'',filterdViolations: []}
+  },
+  watch: {
+    filterText:function(){
+      // console.log(this.filterText)
+      this.filterViolations(this.filterText);
+    }
+  },
+  methods: {
+    filterViolations : function(filter){
+      // var filter = this.filterText;
+      this.filterdViolations = this.violations.filter(function(violation){
+        // console.log(self.filterText)
+        return (!filter || (violation.Name || '').indexOf(filter) != -1);
+      })
+    }
+  },
   components: {
-    HelloWorld
+    // violationList
   }
 }
 </script>
@@ -23,6 +80,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+table{
+  direction: rtl;
+  margin: 24px;
+}
+td{
+  text-align: right !important;
 }
 </style>
---
+++ src/components/violationList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <input type="text">
+        <ul >
+            <li v-for="violation in violations" :key="violation.ID" style="color:red;">
+                {{violation.Name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'violationList',
+  props: {
+    violations: Array
+  },
+  data: function(){
+      return {filterText:''};
+  }
+}
+</script>
